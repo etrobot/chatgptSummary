1634255e32e5b978504a1cbf785411bfcd7a8147
tfproduct01 <etrobot@outlook.com>
2023-03-05 00:41:13 +0800

    update readme

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # chatgptSummary
 
-捕获微信聊天中转发的公众号号文章，并交给chatgpt总结后回复。
+捕获微信聊天中转发的文章，交给chatgpt生成总结。
 
 参考项目：
+
 [BiliGPT 哔哩哔哩 · 视频内容一键总结 b.jimmylv.cn](https://github.com/JimmyLv/BiliGPT)
+
 [chatgpt-on-wechat](https://github.com/zhayujie/chatgpt-on-wechat)
 
+[chatgpt-on-wecom](https://github.com/mostlittlebee/chatgpt-on-wecom)
+
 API：
+
 [ChatGPT](https://github.com/acheong08/ChatGPT)
--- wechat.py
+++ wechat.py
@@ -237,12 +237,12 @@ class weChat():
             else:
                 return False
 
-        bullets = [x for x in query if len(x) >= 2 and checkIndex(x)]
-        bulletsLen = len('\n'.join(bullets))
-        query1 = queryText[:1200 - int(bulletsLen / 2)].split('\n')[:-1]
-        query1.extend(bullets)
-        query1 = [x for x in query1 if x not in queryText[-1200 + int(bulletsLen / 2):]]
-        query1.extend(queryText[-1200 + int(bulletsLen / 2):].split('\n')[1:])
+        keyPoints = [x for x in query if len(x) >= 2 and checkIndex(x)]
+        keyPointsLen = len('\n'.join(keyPoints))
+        query1 = queryText[:1200 - int(keyPointsLen / 2)].split('\n')[:-1]
+        query1.extend(keyPoints)
+        query1 = [x for x in query1 if x not in queryText[-1200 + int(keyPointsLen / 2):]]
+        query1.extend(queryText[-1200 + int(keyPointsLen / 2):].split('\n')[1:])
         query1 = [x.strip() for x in query1 if len(x.strip()) >= 2]
         query = list(set(query1))
         query.sort(key=query1.index)
