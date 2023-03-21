3cf0bedd524e9d8cd192e092837e4ce39425e9a1
tfproduct01 <etrobot@outlook.com>
2023-03-21 18:30:18 +0800

    fix bug

--- wechat.py
+++ wechat.py
@@ -58,7 +58,7 @@ class weChat():
             if msg['MsgType'] == 49:
                 self.msg49(msg)
                 filename = msg['FileName']
-                prompt = '总结要点，带序号:'
+                prompt = '用中文总结要点，带序号:'
                 query = tl.ripPost(filename, tl.posts.df)
             elif '[Link]' in content or '[链接]' in content:
                 filename = tl.extractWxTitle(content)
@@ -89,7 +89,7 @@ class weChat():
             return
         if '[Message cannot be displayed]' in msg['Content']:
             query=tl.dealText(tl.ripBili(tl.posts.df[tl.posts.df['Url'].str.contains('23.tv')]['Url'].iloc[-1]))
-            tl.thread_pool.submit(self._do_send_group, query, msg, msg['FileName'], '总结要点，带序号：')
+            tl.thread_pool.submit(self._do_send_group, query, msg, msg['FileName'], '用中文总结要点，带序号：')
             return
 
         quote = '\n- - - - - - - - - - - - - - -\n'
@@ -124,8 +124,8 @@ class weChat():
             if len(query)>2000 or '总结' in prompt:
                 queryText = queryText +'\nTL;DR;Use Chinese.'
             reply_text = self.chatBot.reply(queryText,context)
-            if reply_text:
-                self.send(tl.conf.get("single_chat_reply_prefix") + reply_text, reply_user_id)
+            if reply_text is not None:
+                self.send('[LLM]' + reply_text, reply_user_id)
                 if title != '' and title in tl.posts.df.index and self.chatBot.state == 'complete':
                     tl.posts.update(key=title,field= 'Summary',content=reply_text)
                 
@@ -136,7 +136,7 @@ class weChat():
         if not query:
             return
         if title !='' and title in tl.posts.df.index and tl.posts.df.loc[title]['Summary'] != '' and ('总结' in prompt or prompt==''):
-            query = tl.posts.df.loc[title]['Summary'].split('[ChatGPT]')[-1]
+            query = tl.posts.df.loc[title]['Summary'].split('[LLM]')[-1]
             self.send(query, msg['User']['UserName'])
             return
         context = dict()
@@ -146,7 +146,7 @@ class weChat():
         if len(prompt) < 4 or len(query) > 600:
             query = query + '\nTL;DR; Use Chinese.'
         reply_text = self.chatBot.reply(query, context)
-        if reply_text:
+        if reply_text is not None:
             self.send('@' + msg['ActualNickName'] + ' ' + reply_text.strip(), group_id)
             if title != '' and title in tl.posts.df.index and self.chatBot.state=='complete':
                 tl.posts.update(key=title,field= 'Summary',content=reply_text)
