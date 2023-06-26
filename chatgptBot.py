7deab780af829a15733b61a92c9fd91b8833fa3c
Frank Lin <etrobot@outlook.com>
2023-06-26 12:18:04 +0800

    update api usage

--- chatgptBot.py
+++ chatgptBot.py
@@ -1,18 +1,19 @@
 import commonTools as tl
 import json,re
-from EdgeGPT import Chatbot, ConversationStyle
+from EdgeGPT.EdgeGPT import Chatbot, ConversationStyle
 import poe
 import asyncio
 
 class Bing():
     def __init__(self):
-        self.bot = asyncio.run(Chatbot.create(cookie_path='./cookies.json',proxy=tl.conf.get('proxy')))
+        cookies = json.loads(open("./cookies.json", encoding="utf-8").read())
+        self.bot = asyncio.run(Chatbot.create(cookies=cookies, proxy=tl.conf.get('proxy')))
 
     def reply(self, querytext:str):
         reply_text=None
         reply = asyncio.run(self.bot.ask(prompt=querytext, conversation_style=ConversationStyle.precise, wss_link="wss://sydney.bing.com/sydney/ChatHub"))
         if reply:
-            reply_text=reply["item"]["messages"][1]["adaptiveCards"][0]["body"][0]["text"]
+            reply_text=reply["item"]["messages"][-1]["adaptiveCards"][0]["body"][0]["text"]
             reply_text=re.sub(r'\[\^[0-9]+\^\]', '', reply_text)
         return reply_text
 
